7a374779b2c593c52023092505597c45712cf045
Juniooor <juniormodern@hotmail.com>
2023-04-24 21:26:27 -0300

    configurando novo app

--- comentarios/models.py
+++ comentarios/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Comentario(models.Model):
@@ -9,4 +9,4 @@ class Comentario(models.Model):
     aprovado = models.BooleanField(default=True)
     
     def __str__(self):
-        return self.usuario.first_name
+        return self.usuario.username
--- core/models.py
+++ core/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+
 from atracoes.models import Atracao
+from avaliacoes.models import Avaliacao
+from comentarios.models import Comentario
+
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
-    Atracao = models.ManyToManyField(Atracao)
+    atracoes = models.ManyToManyField(Atracao, related_name='pontos_turisticos')
+    comentarios = models.ManyToManyField(Comentario, related_name='pontos_turisticos')
+    avaliacao = models.ManyToManyField(Avaliacao)
     
     def __str__(self):
         return self.nome    
