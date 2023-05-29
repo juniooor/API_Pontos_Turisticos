d869e0e114b66f716269961107190eda450d8099
Juniooor <juniormodern@hotmail.com>
2023-05-29 20:21:32 -0300

    implementando autenticação pt 2

--- core/api/viewset.py
+++ core/api/viewset.py
@@ -1,4 +1,6 @@
 from rest_framework import filters
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import PontoTuristico
@@ -9,6 +11,8 @@ from .serializers import PontoTuristicoSerializer
 class PontoTuristicoViewset(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     filter_backends = [filters.SearchFilter]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
     search_fields = ["aprovado"]
 
     def get_queryset(self):
