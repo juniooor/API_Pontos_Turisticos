a46039adcff434cdde0bf609e19f3e0f803e66e9
Juniooor <juniormodern@hotmail.com>
2023-05-29 19:30:01 -0300

    implementando autenticação

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for pontos_turisticos project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from atracoes.api.viewset import AtracaoViewset
 from avaliacoes.api.viewset import AvaliacaoViewset
@@ -35,5 +20,6 @@ router.register(r"enderecos", EnderecoViewset)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("api-token-auth/", obtain_auth_token),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
