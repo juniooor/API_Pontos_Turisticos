2ec0d5030624a4a2c6ef9589e1068f83baf88e7a
juniooor <juniormodern@hotmail.com>
2023-06-21 21:32:35 -0300

    alterando permission class

--- core/api/viewset.py
+++ core/api/viewset.py
@@ -1,6 +1,6 @@
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import PontoTuristico
@@ -11,7 +11,7 @@ from .serializers import PontoTuristicoSerializer
 class PontoTuristicoViewset(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     filter_backends = [filters.SearchFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     authentication_classes = [TokenAuthentication]
     search_fields = ["aprovado"]
 
